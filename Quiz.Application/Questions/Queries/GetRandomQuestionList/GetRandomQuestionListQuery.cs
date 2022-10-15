4ba9e20d5f356adb95fa28334a88f5b380f12d8e
99Astral99 <strik4462@gmail.com>
2022-10-15 22:04:54 +0300

    Added GetRandomQuestionList query

---
+++ Quiz.Application/Questions/Queries/GetRandomQuestionList/GetRandomQuestionListQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Quiz.Application.Questions.Queries.GetQuestionList;
+
+namespace Quiz.Application.Questions.Queries.GetRandomQuestionList
+{
+    public class GetRandomQuestionListQuery : IRequest<QuestionListVm>
+    {
+        //Plug class
+    }
+}
---
+++ Quiz.Application/Questions/Queries/GetRandomQuestionList/GetRandomQuestionListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Quiz.Application.Interfaces;
+using Quiz.Application.Questions.Queries.GetQuestionList;
+
+namespace Quiz.Application.Questions.Queries.GetRandomQuestionList
+{
+    public class GetRandomQuestionListQueryHandler
+        : IRequestHandler<GetRandomQuestionListQuery, QuestionListVm>
+    {
+        private readonly IQuizDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public GetRandomQuestionListQueryHandler(IQuizDbContext dbContext, IMapper mapper) =>
+            (_dbContext, _mapper) = (dbContext, mapper);
+        public async Task<QuestionListVm> Handle(GetRandomQuestionListQuery request, CancellationToken cancellationToken)
+        {
+            //Take random 10 questions from the list
+            var questionsQuery = await _dbContext.Questions
+                .ProjectTo<QuestionLookupDto>(_mapper.ConfigurationProvider)
+                .OrderBy(arg => Guid.NewGuid())
+                .Take(10)
+                .ToListAsync();
+
+            return new QuestionListVm { Questions = questionsQuery };
+        }
+    }
+}
