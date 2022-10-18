def677fc142de76e93bb4a95ae76e9f2e137028e
99Astral99 <strik4462@gmail.com>
2022-10-18 20:06:04 +0300

    Added CORS / Dependency Injection

--- Quiz.WebApi/Program.cs
+++ Quiz.WebApi/Program.cs
@@ -1,4 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
